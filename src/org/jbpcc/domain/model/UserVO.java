f47d82e4cb40ed5288c7e8cd2ba0500c23e2a0f6
khoo.james <khoo.james@25298682-f348-0410-9b87-adbb87aa2d7a>
2008-03-24 07:28:52 +0000

    Migrate two value objects

---
+++ src/org/jbpcc/domain/model/UserVO.java
@@ -0,0 +1,83 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jbpcc.domain.model;
+
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+
+/**
+ * A VO to store JBPCC login user data
+ * @version $Revision:  $
+ */
+public class UserVO extends ValueObject {
+    private String loginName;
+    private String password;
+
+    public UserVO() {
+    }
+
+    public UserVO(String loginName, String password) {
+        this.loginName = loginName;
+        this.password = password;
+    }
+    
+    
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
+   @Override
+    public ValueObject clone() throws CloneNotSupportedException {
+        UserVO clone = (UserVO) super.clone();
+        return clone;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+}
